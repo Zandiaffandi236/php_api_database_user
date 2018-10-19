c07a5d09df3911363043649edaf836b19cc64d03
Zandiaffandi236 <marzandi.leta@gmail.com>
2018-10-19 13:01:44 +0700

    add value table

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost"; //host database
+    $username = "root";  //username database
+    $password = "";
+    $database ="php-api";
+    
+
+    $koneksi = mysqli_connect ($host, $username, $password, $database);
+
+    
+?>
